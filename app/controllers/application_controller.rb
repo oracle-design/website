e863e29d72ef3a6b2a299c2b5d2c3c1643d08cd9
FunnyQ <funnyq@funnyq.com>
2015-09-01 01:41:00 +0800

    [add] adding member info edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   layout :layout_by_resource
 
   def current_member
@@ -18,4 +20,15 @@ class ApplicationController < ActionController::Base
       "application"
     end
   end
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit(:name, :message, :avatar, :work_field,
+          :email, :password, :password_confirmation)
+      end
+      devise_parameter_sanitizer.for(:account_update) do |u|
+        u.permit(:name, :message, :avatar, :work_field,
+          :email, :password, :password_confirmation, :current_password)
+      end
+    end
 end
