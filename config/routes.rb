c90a64f909c2ea43bbf78447a096a0f5225450c7
FunnyQ <funnyq@funnyq.com>
2015-08-31 22:38:07 +0800

    [add] adding member model via devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :members
   root 'prototype#index'
   resources :service_forms, only: [:create]
   # The priority is based upon order of creation: first created -> highest priority.
